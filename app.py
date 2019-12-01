847c656ab04f7e1a01918736ae187f24b0029ec4
Anishka <anishka.paharia@gmail.com>
2019-11-30 21:44:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ def test_rec():
 if __name__ == "__main__":
  
     
+ app.run("localhost", "9999", debug=True)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+#port = int(os.environ.get("PORT", 5000))
+#app.run(debug=True, host='0.0.0.0', port=port)
